6f75f9886a83d735c66495901bbf6c894b6c05d1
Jacqueline Mckay H <jacquelinemc@outlook.es>
2024-04-16 09:18:02 -0600

    test first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WeatherApp from './components/weatherApp'
+
+function App() {
+  return (
+    <>
+      <WeatherApp/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/weatherApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WeatherApp = () => {
+  return(
+  <>
+  	<h1>this is a title</h1>
+  </>
+  )
+}
+
+export default WeatherApp;
