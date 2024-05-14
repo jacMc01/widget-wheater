5449db85b7e0bb33e7c8e46b277d19403e539d81
jvaldiviezo9 <jvaldiviezo@hotmail.co.uk>
2024-05-14 13:51:12 -0600

    fix: demo/working

--- src/components/weatherApp.jsx
+++ src/components/weatherApp.jsx
@@ -1,36 +1,53 @@
 import React, { useState } from "react";
 import WeatherForm from "./weatherForm";
-import process from "process";
+
+
 
 const WeatherApp = () => {
-
   const [weather, setWeather] = useState(null);
+  
+  async function loadInfo(city = "London") {
+    console.log("loadInfo");
+    console.log(city);
+    console.log(import.meta.env.VITE_APP_URL);
+    console.log(import.meta.env.VITE_APP_KEY);
+    
+    try {
+      const api_str = `${import.meta.env.VITE_APP_URL}&key=${import.meta.env.VITE_APP_KEY}&q=${city}`;
+      console.log(api_str);
 
-  async function loadInfo(city = "london"){
-    try{
-      const request = await fetch(`${process.env.REACT_APP_URL}&key${process.env.REACT_APP_KEY}&q=${city}`);
+      const request = await fetch(api_str);
 
       const json = await request.json();
+      setWeather(json); // Update the state with the fetched weather data
       console.log(json);
 
-    } catch(error){
-
+    } catch (error) {
+      console.log("Error description:");
+      console.log(error);
     }
   }
 
-  function handleChangeCity(city){
+  function handleChangeCity(city) {
     setWeather(null);
-
     loadInfo(city);
   }
 
-
-  return(
+  return (
     <>
-      <WeatherForm onChangeCity={handleChangeCity}/>
-      <div className="info">Info</div>
+      <WeatherForm onChangeCity={handleChangeCity} />
+      <div className="info">
+        {weather ? (
+          <div>
+            <h3>Weather Info:</h3>
+            <p>{JSON.stringify(weather)}</p> {/* Display weather information */}
+          </div>
+        ) : (
+          "No weather information available"
+        )}
+      </div>
     </>
-  )
+  );
 }
 
 export default WeatherApp;
